2993d7a07c985a8f1ac37de37d0019c55c2eb3ac
dorianguzman <dorianeguzman@gmail.com>
2024-02-08 00:47:57 +0100

    Last_version
    Last version of the files

--- source/tools.py
+++ source/tools.py
@@ -1,14 +1,16 @@
 # -*- coding: utf-8 -*-
 """
-Created on Tue Oct 24
+Created on Feb 07
 
 @author: DorianGuzman
 """
-import pandas as pd
+
 import os
+import pandas as pd
+from pytz import timezone
 
 
-def read_data(filename: str)->pd.DataFrame:
+def read_data(filename: str) -> pd.DataFrame:
     """
     Read data from a CSV file and preprocess it for analysis.
 
@@ -22,32 +24,32 @@ def read_data(filename: str)->pd.DataFrame:
     pd.DataFrame
         A DataFrame with preprocessed data for analysis.
     """
-
+    
     # Check if the file exists
     if not os.path.isfile(filename):
         raise FileNotFoundError(f"File not found: {filename}")
 
-    # Define the data types for specific columns
-    dtype_mapping = {
-        'data_date': 'str',
-        'wind_speed': 'float32',
-        'tmp_amb': 'float32',
-        'poa': 'float32',
-        'tmp_cell': 'float32',
-        'ac_power': 'float32',
-        'rain_mm': 'float32'
-    }
+    # Read the CSV file
+    df = pd.read_csv(filename, delimiter=';')
 
-    # Read the CSV file into a DataFrame with predefined data types
-    df = pd.read_csv(filename, dtype=dtype_mapping, parse_dates=['data_date'])
-    # Rename columns
-    df = df.rename(columns={'data_date': 'datetime'})
-    
-    # Set the 'datetime' column as the DataFrame index
-    df.set_index('datetime', drop=True, inplace=True)
-    
-    # Convert 'ac_power' to watts
-    df['ac_power'] *= 1000
+    # Convert the 'Datum' column to a datetime object and set it as the index
+    df['Datum'] = pd.to_datetime(df['Datum'], format='%d.%m.%Y %H:%M')
+    europe_timezone = timezone('Europe/Berlin')  # Assuming Central Europe time zone
+    df['Datum'] = df['Datum'].dt.tz_localize(europe_timezone)
+    df.set_index('Datum', inplace=True)
+
+    # Rename the columns by adding 'String' to each column name
+    df.columns = [f'String{col}' for col in df.columns]
+
+    # Replace ',' by '.'
+    # Some values have , instead of ., therefore this has to be 
+    # replaced, and converted back to numerical values
+    # Otherwise, values that cannot be converted to numeric will be
+    # replaced by NaN. 
+    df.replace(',', '.', regex=True, inplace=True)
+
+    # Convert columns to numeric
+    df = df.apply(pd.to_numeric, downcast="float", errors='coerce').round(2)
 
     return df
 
